9a506957b49af6fb386af4e2cabe0a9e10d13275
Gilangkw <32380279+Gilangkw@users.noreply.github.com>
2017-11-14 22:44:38 +0700

    Add files via upload

---
+++ PBO11_10116506_LatihanUTS1/src/LatihanUTS1_10116506/IF11_10116506_LatihanUTS1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanUTS1_10116506;
+
+/**
+ *
+ * @author gilang kusuma wijaya
+ */
+public class IF11_10116506_LatihanUTS1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
